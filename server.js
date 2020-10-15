d6d28c4e8dec9ef9f42382dfbd860996dac1f322
airmonger <aravindkrishnanparayil@gmail.com>
2020-10-15 09:53:00 +0530

    connecting database

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const image =require('./controller/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'vind',
-    database : 'face-detection'
+	  connectionString: process.env.DATABASE_URL,
+	  ssl: {
+	    rejectUnauthorized: false
+	  }  
   }
 });
 
